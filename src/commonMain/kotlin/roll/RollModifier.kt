820c5862259e7ca479debdad55e7317a109f8944
Alex Chauvin <alex@chauvin.dev>
2021-05-19 19:33:06 -0400

    fix: Make reason parameter nullable
    Signed-off-by: Alex Chauvin <alex@chauvin.dev>

--- src/commonMain/kotlin/roll/RollModifier.kt
+++ src/commonMain/kotlin/roll/RollModifier.kt
@@ -2,9 +2,9 @@ package roll
 
 /**
  * A RollModifier is a combination of an integer [value] that is to be applied to a die roll,
- * along with the [reason] for this modifier.
+ * along with an optional [reason] for this modifier.
  */
 data class RollModifier(
     val value: Int,
-    val reason: String
+    val reason: String? = null
 )
