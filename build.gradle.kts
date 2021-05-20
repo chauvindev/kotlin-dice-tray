84eb9927c4b23e98a89982404dbc2d063806b785
Alex Chauvin <alex@chauvin.dev>
2021-05-20 15:36:57 -0400

    refactor: Use Snapshot versions until first release
    Signed-off-by: Alex Chauvin <alex@chauvin.dev>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,16 @@
 plugins {
     kotlin("multiplatform") version "1.5.0"
     id("org.jetbrains.dokka") version "1.4.32"
+    id("maven-publish")
 }
 
 group = "dev.chauvin"
-version = "1.0.0"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
+
 
 kotlin {
     jvm {
