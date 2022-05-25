fc3e018a35fabf35700c99a2a32bb26271964f5d
Alex Chauvin <alex@chauvin.dev>
2022-05-25 17:44:48 -0400

    refactor: Make RollResult a data class with generic type

--- src/commonMain/kotlin/dev/chauvin/dicetray/roll/RollResult.kt
+++ src/commonMain/kotlin/dev/chauvin/dicetray/roll/RollResult.kt
@@ -1,11 +1,18 @@
 package dev.chauvin.dicetray.roll
 
 /**
+ * Represents the result of a die roll.
+ *
+ * Each instance of the RollResult class holds the [value] obtained by rolling a die.
+ * For convenience, the RollResult instance also holds the [rawValue] of the die roll,
+ * along with a copy of all the [modifiers] that were applied to this [rawValue] to
+ * compute [value].
+ *
  * The RollResult interface represents the outcome of a roll and must, at minimum,
  * hold the [value] of the roll.
  */
 public data class RollResult<T: Any>(
     public val value: T,
-    public val modifiers: List<RollModifier<T>>?,
+    public inline val modifiers: List<RollModifier<T>>?,
     public val rawValue: T
 )
