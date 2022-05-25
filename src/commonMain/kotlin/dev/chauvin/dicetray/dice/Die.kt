e1995bf74cc2fdaad59284e4f3b12246623cf2ae
Alex Chauvin <alex@chauvin.dev>
2022-05-25 17:48:39 -0400

    docs: Update documentation

--- src/commonMain/kotlin/dev/chauvin/dicetray/dice/Die.kt
+++ src/commonMain/kotlin/dev/chauvin/dicetray/dice/Die.kt
@@ -4,15 +4,14 @@ import dev.chauvin.dicetray.roll.RollModifier
 import dev.chauvin.dicetray.roll.RollResult
 
 /**
- * Die defines the basic properties and functionality needed to create a rollable die with
- * a number of [faces].
+ * Die defines the basic properties and functionality needed to create a die with an arbitrary number of [faces].
  */
 public interface Die <T: Any> {
 
     public val faces: List<Face<T>>
 
     /**
-     * Roll the die and return the result.
+     * Roll the die a number of times equal to [numberOfRolls]. For each die rolled, apply [rollModifiers].
      */
     public fun roll(numberOfRolls: Int = 1, rollModifiers: List<RollModifier<T>> = emptyList()): List<RollResult<T>>
 }
